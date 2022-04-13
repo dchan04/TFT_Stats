aeae16c89d95d47372bfebb5c1d465fc1ab21b96
dchan04 <47226733+dchan04@users.noreply.github.com>
2022-04-13 17:01:27 -0400

    Implemented sorting functionality. You can now search for little legends by name and by species

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Diagnostics;
 using System.Linq;
@@ -24,20 +25,62 @@ namespace TFT_Stats.Controllers
             return View();
         }
 
-        public IActionResult CompanionView(string sortOrder)
+        public IActionResult CompanionView(string sortOrder, string currentFilter, string searchString, string speciesFilter)
         {
+            Console.WriteLine($"{speciesFilter}");
+            //Console.WriteLine($"{currentFilter}");
+            Console.WriteLine($"{searchString}");
             ViewBag.NameSortParm = sortOrder == "name_ascen" ? "name_desc" : "name_ascen";
             ViewBag.Species = sortOrder == "species_ascen" ? "species_desc" : "species_ascen";
             ViewBag.Count = String.IsNullOrEmpty(sortOrder) ? "count_ascen" : "";
-            return sortOrder switch
+            var speciesQuery = _context.CompanionViewModel.OrderBy(c => c.Species).Select(c => c.Species).Distinct().ToList();
+            ViewBag.speciesList = new SelectList(speciesQuery);
+            var companionList = _context.CompanionViewModel.AsQueryable();
+            if (searchString == null)
             {
-                "name_desc" => View(_context.CompanionViewModel.ToList().OrderByDescending(c => c.Name)),
-                "species_desc" => View(_context.CompanionViewModel.ToList().OrderByDescending(c => c.Species)),
-                "name_ascen" => View(_context.CompanionViewModel.ToList().OrderBy(c => c.Name)),
-                "species_ascen" => View(_context.CompanionViewModel.ToList().OrderBy(c => c.Species)),
-                "count_ascen" => View(_context.CompanionViewModel.ToList().OrderBy(c => c.Count)),
-                _ => View(_context.CompanionViewModel.ToList().OrderByDescending(c => c.Count)),
-            };
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
+
+            if (!String.IsNullOrEmpty(searchString) && !String.IsNullOrEmpty(speciesFilter))
+            {
+                companionList = companionList.Where(c => c.Name == searchString && c.Species == speciesFilter);
+            }
+            else if (String.IsNullOrEmpty(searchString) && !String.IsNullOrEmpty(speciesFilter))
+            {
+                companionList = companionList.Where(c => c.Species == speciesFilter);
+            }
+            else if (!String.IsNullOrEmpty(searchString) && String.IsNullOrEmpty(speciesFilter))
+            {
+                companionList = companionList.Where(c => c.Name == searchString);
+            }
+            else
+            {
+                //nothing
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    companionList = companionList.OrderByDescending(c => c.Count);
+                    break;
+                case "species_desc":
+                    companionList = companionList.OrderByDescending(c => c.Species);
+                    break;
+                case "name_ascen":
+                    companionList = companionList.OrderBy(c => c.Name);
+                    break;
+                case "species_ascen":
+                    companionList = companionList.OrderBy(c => c.Species);
+                    break;
+                case "count_ascen":
+                    companionList = companionList.OrderBy(c => c.Count);
+                    break;
+                default:
+                    companionList = companionList.OrderByDescending(c => c.Count);
+                    break;
+            }
+            return View(companionList);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
